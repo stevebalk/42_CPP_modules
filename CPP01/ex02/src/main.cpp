7ce10ac18704dde205cfa27a2f14ba2f3a20cc56
stevebalk <steve.balk@web.de>
2024-02-08 14:11:47 +0100

    Finished ex02

--- CPP01/ex02/src/main.cpp
+++ CPP01/ex02/src/main.cpp
@@ -1,5 +1,16 @@
+#include <iostream>
 #include <string>
 
 int main() {
+	std::string ibrain = "HI THIS IS BRAIN";
+	std::string *str_pointer = &ibrain;
+	std::string &str_reference = ibrain;
 
+	std::cout << &ibrain << std::endl;
+	std::cout << str_pointer << std::endl;
+	std::cout << &str_reference << std::endl;
+
+	std::cout << ibrain << std::endl;
+	std::cout << *str_pointer << std::endl;
+	std::cout << str_reference << std::endl;
 }
