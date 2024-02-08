54e09de204400f5ce8963a7b5ac1271ffd203c1e
stevebalk <steve.balk@web.de>
2024-02-08 17:03:17 +0100

    Renamed varibles to match exercise

--- CPP01/ex02/src/main.cpp
+++ CPP01/ex02/src/main.cpp
@@ -3,14 +3,14 @@
 
 int main() {
 	std::string ibrain = "HI THIS IS BRAIN";
-	std::string *str_pointer = &ibrain;
-	std::string &str_reference = ibrain;
+	std::string *stringPTR = &ibrain;
+	std::string &stringREF = ibrain;
 
 	std::cout << &ibrain << std::endl;
-	std::cout << str_pointer << std::endl;
-	std::cout << &str_reference << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
 
 	std::cout << ibrain << std::endl;
-	std::cout << *str_pointer << std::endl;
-	std::cout << str_reference << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
 }
