0c2643bca319449c493e3286f065d284b5537268
stevebalk <steve.balk@web.de>
2024-02-13 12:41:57 +0100

    Finished ex05

--- CPP01/ex05/include/Harl.hpp
+++ CPP01/ex05/include/Harl.hpp
@@ -1,12 +1,15 @@
 #ifndef HARL_HPP
 #define HARL_HPP
 
+#include <iostream>
 #include <string>
 
 class Harl {
-
 public:
 	void complain(std::string level);
+
+	Harl();
+	~Harl();
 
 private:
 	void debug(void);
---
+++ CPP01/ex05/src/Harl.cpp
@@ -0,0 +1,48 @@
+#include "Harl.hpp"
+
+Harl::Harl() {
+	std::cout << "Harl enters the restaurant and takes a seat." << std::endl;
+}
+
+Harl::~Harl() {
+	std::cout << "Harl leaves the restaurant." << std::endl;
+}
+
+void Harl::debug() {
+	std::cout << "I love having extra bacon for my "
+			  << "7XL-double-cheese-triple-pickle-special- "
+			  << "ketchup burger. I really do!"
+			  << std::endl;
+}
+
+void Harl::info() {
+	std::cout << "I cannot believe adding extra bacon costs more money. "
+			  << "You didn’t put "
+			  << "enough bacon in my burger! If you did, I wouldn’t be "
+			  << "asking for more! "
+			  << std::endl;
+}
+
+void Harl::warning() {
+	std::cout << "I think I deserve to have some extra bacon for free. "
+			  << "I’ve been coming for "
+			  << "years whereas you started working here since last month."
+			  << std::endl;
+}
+
+void Harl::error() {
+	std::cout << "This is unacceptable! I want to speak to the manager now."
+			  << std::endl;
+}
+
+void Harl::complain(std::string level) {
+	const std::string complains[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+	void (Harl::*func_ptr[4])() = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
+	for (int i = 0; i < 4; i++) {
+		if (complains[i].compare(level) == 0) {
+			(this->*func_ptr[i])();
+			return;
+		}
+	}
+	std::cout << "Harl is speechless!" << std::endl;
+}
