70c8f36e34bc15c4b0fe4b7416a53cd77ed5929a
stevebalk <steve.balk@web.de>
2024-02-19 14:15:19 +0100

    Finished ex00

---
+++ CPP03/ex00/src/main.cpp
@@ -0,0 +1,25 @@
+#include "ClapTrap.hpp"
+
+int main() {
+	ClapTrap cla4p_tp;
+	ClapTrap bob("Bob");
+	ClapTrap dave("Dave");
+
+	{
+		for (int i = 0; i < 11; i++) {
+		cla4p_tp.attack("Barrel");
+		}
+	}
+	{
+		std::cout << std::endl;
+		for (int i = 0; i < 11; i++) {
+			bob.beRepaired(1);
+		}
+		bob.attack("the air");
+		std::cout << std::endl;
+		for (int i = 0; i < 21; i++) {
+			bob.takeDamage(1);
+		}
+	}
+	std::cout << std::endl;
+}
