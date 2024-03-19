3d25ba46d55eefb1ddabf44cea25bf32e15731ac
stevebalk <steve.balk@web.de>
2024-03-19 10:31:26 +0100

    Finished

--- CPP09/ex01/src/RPN.cpp
+++ CPP09/ex01/src/RPN.cpp
@@ -1,102 +1,113 @@
 #include <RPN.hpp>
 
-RPN::RPN() {}
+/********************************************************************/
+/*                          MEMBER FUNCTIONS                        */
+/********************************************************************/
 
-RPN::RPN(const RPN &other) {
-	if (this != &other) {
-		_number_stack = other._number_stack;
-	}
-}
-
-RPN::~RPN() {}
-
-RPN &RPN::operator=(const RPN &rhs) {
-	_number_stack = rhs._number_stack;
-	return *this;
+bool RPN::isNumber(const std::string &str) {
+	std::stringstream ss(str);
+	int64_t f;
+	ss >> f;
+	// Check if the entire string was consumed and if there's no error
+	return (ss.eof() && !ss.fail());
 }
 
 void RPN::calculate(const std::string equation) {
 	std::stringstream ss(equation);
+	std::string token;
 
-	int64_t num;
-
-	// ss >> std::skipws >> num;
-	// if (ss.fail()) {
-	// 	throw InvalidEquation();
-	// }
-	// _number_stack.push(num);
-	while (ss.eof() == false) {
-		ss >> num;
-		if (ss.fail()) {
-			ss.clear();
-			std::cout << "Fail" << std::endl;
-			char token;
-			ss >> std::skipws >> token;
-			std::cout << "Char: " << static_cast<int>(token) << std::endl;
-			if (token == '+' || token == '-' || token == '*' || token == '/') {
-				_token_stack.push(token);
-				if (_number_stack.size() >= _token_stack.size()) {
-					throw InvalidEquation();
-				} else if (_number_stack.size() - 1 == _token_stack.size()) {
-					calculateStacks();
-				}
-			} else {
+	while (std::getline(ss, token, ' ')) {
+		if (isNumber(token)) {
+			_number_stack.push(stot<int64_t>(token));
+		} else {
+			if (token.size() != 1 || (token[0] != '+' && token[0] == '-' && token[0] == '*' && token[0] == '/')) {
 				throw InvalidEquation();
 			}
-		} else {
-			std::cout << num << std::endl;
-			_number_stack.push(num);
+			if (_number_stack.size() < 2) {
+				throw InvalidEquation();
+			}
+
+			int64_t num2 = _number_stack.top();
+			_number_stack.pop();
+			int64_t num1 = _number_stack.top();
+			_number_stack.pop();
+
+			if (token == "+")
+				_number_stack.push(num1 + num2);
+			else if (token == "-")
+				_number_stack.push(num1 - num2);
+			else if (token == "*")
+				_number_stack.push(num1 * num2);
+			else if (token == "/") {
+				if (num2 == 0)
+					throw std::runtime_error("Error: zero division");
+				else
+					_number_stack.push(num1 / num2);
+			}
 		}
 	}
-
-	if (_number_stack.size() == 1) {
-		std::cout << _number_stack.top() << std::endl;
-	} else {
+	if (_number_stack.size() != 1)
 		throw InvalidEquation();
+
+	int64_t result = _number_stack.top();
+	_number_stack.pop();
+	std::cout << result << std::endl;
+}
+
+/********************************************************************/
+/*                          CONSTRUCTOR                             */
+/********************************************************************/
+
+RPN::RPN() {}
+
+/********************************************************************/
+/*                          DECONSTRUCTOR                           */
+/********************************************************************/
+
+RPN::~RPN() {}
+
+/********************************************************************/
+/*                          COPY CONSTRUCTOR                        */
+/********************************************************************/
+
+RPN::RPN(const RPN &other) {
+	*this = other;
+}
+
+/********************************************************************/
+/*                          OPERATOR OVERLOAD                       */
+/********************************************************************/
+
+RPN &RPN::operator=(const RPN &rhs) {
+	if (this != &rhs) {
+		_number_stack = rhs._number_stack;
 	}
+	return *this;
 }
 
-void RPN::calculateStacks() {
-	for (size_t i = 0; i < _token_stack.size(); i++) {
-		int64_t a = _number_stack.top();
-		_number_stack.pop();
-		int64_t b = _number_stack.top();
-		_number_stack.pop();
-
-		switch (_token_stack.top()) {
-			case '+':
-				_number_stack.push(add(a, b));
-			case '-':
-				_number_stack.push(subtract(a, b));
-			case '*':
-				_number_stack.push(multiply(a, b));
-			case '/':
-				_number_stack.push(divide(a, b));
-		}
-		_token_stack.pop();
-	}
-}
-
-template <typename T>
-T RPN::add(T a, T b) {
-	return (a + b);
-}
-
-template <typename T>
-T RPN::subtract(T a, T b) {
-	return (a - b);
-}
-
-template <typename T>
-T RPN::multiply(T a, T b) {
-	return (a * b);
-}
-
-template <typename T>
-T RPN::divide(T a, T b) {
-	return (a / b);
-}
+/********************************************************************/
+/*                          EXCEPTIONS                              */
+/********************************************************************/
 
 const char *RPN::InvalidEquation::what() const throw() {
 	return "Error";
+}
+
+/********************************************************************/
+/*                          TEMPLATES                               */
+/********************************************************************/
+
+// Reads the string and tries to convert it to T
+template <typename T>
+T RPN::stot(const std::string &str) {
+	std::stringstream ss(str);
+	T f;
+	if (!(ss >> f)) {
+		throw RPN::InvalidEquation();
+	}
+	std::string remaining;
+	if (ss >> remaining) {
+		throw RPN::InvalidEquation();
+	}
+	return f;
 }
